0b4a4f4e43d044bdcd75ff6a6c0d808c7d67a19d
PURAT <alex-shtunder@yandex.ru>
2019-11-09 17:30:44 +0300

    Event update

--- src/main/java/com/baltichack/view/service/EventService.java
+++ src/main/java/com/baltichack/view/service/EventService.java
@@ -1,17 +1,27 @@
 package com.baltichack.view.service;
 
 import com.baltichack.view.entity.Event;
+import com.baltichack.view.entity.User;
 import com.baltichack.view.repos.EventRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.baltichack.view.service.UserService.flag;
+
 @Service
 public class EventService {
 
+    public static List<Event> list = new ArrayList<>();
     @Autowired
     private EventRepo eventRepo;
 
     public void addEvent(Event event) {
+        if (flag) {
+            list.add(event);
+        }
 //        eventRepo.save(event);
     }
 
@@ -20,6 +30,12 @@ public class EventService {
     }
 
     public void removeEvent(Long id) {
+        if(flag) {
+            for (Event event : list) {
+                if (id == (long) event.getId())
+                    list.remove(event);
+            }
+        }
 //        eventRepo.deleteById(id);
     }
 }
